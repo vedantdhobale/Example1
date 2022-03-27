ddcba60e840a1997786af677a63a838fda293b21
vedantdhobale <vedantdhobale1998@gmail.com>
2022-03-27 13:13:37 +0530

    file commited

---
+++ src/main/java/com/training/BookOrderServiceApplication.java
@@ -0,0 +1,36 @@
+package com.training;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.training.config.MyConfiguration;
+
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication
+public class BookOrderServiceApplication {
+
+	public static void main(String[] args) {
+SpringApplication.run(BookOrderServiceApplication.class, args);
+	}
+	
+	
+	@Autowired
+	private MyConfiguration config;
+	
+	
+	
+	  @Bean CommandLineRunner getRunner(ApplicationContext ctx) { return args->{
+	  
+	  System.out.println("Url:"+config.getUrl()); }; }
+	 
+
+
+
+}
